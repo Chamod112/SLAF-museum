fbe5425e525a14b9bf6c925806fbe5befa530d28
Chamod112 <chamodnissanka36@gmail.com>
2026-04-17 13:49:18 +0530

    Updated contact section with new content and improved layout for better user engagement.

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+
+$pageTitle  = "The Sri Lanka Air Force Museum";
+$activePage = "contact";
+
+require_once 'sections/head.php';
+require_once 'sections/navbar.php';
+require_once 'sections/contact-hero.php';
+require_once 'sections/contact-main.php';
+require_once 'sections/contact-right.php';
+require_once 'sections/contact-left.php';
+require_once 'sections/footer.php';
+?>
